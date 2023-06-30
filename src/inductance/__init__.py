3fcd5461d972a0f4c9851b5eb744122417b49503
Darren Garnier <dgarnier@reinrag.net>
2023-06-30 19:07:40 -0400

    git rid of mistaken hard coded versioning (#22)

--- src/inductance/__init__.py
+++ src/inductance/__init__.py
@@ -1,6 +1,9 @@
 """Inductance."""
 
 # this gets replaced with the package version during the build process
-__version__ = "0.0.0"
+try:
+    from ._version import __version__  # type: ignore
+except ImportError:
+    __version__ = "0.0.0+unknown"
 
 # from . import elliptics, filaments, inductance
