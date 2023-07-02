2b01a2ac4536599e7588d45cf8310d19b2280b8d
Darren Garnier <dgarnier@reinrag.net>
2023-07-02 13:37:30 -0400

    use static versioning

--- src/inductance/__init__.py
+++ src/inductance/__init__.py
@@ -1,9 +1,7 @@
 """Inductance."""
 
-# this gets replaced with the package version during the build process
-try:
-    from ._version import __version__  # type: ignore
-except ImportError:
-    __version__ = "0.0.0+unknown"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("inductance")
 
 # from . import elliptics, filaments, inductance
