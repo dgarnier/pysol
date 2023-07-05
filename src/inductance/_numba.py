920e5ad2f14e96aa82ba698ea044f41b960bb438
Darren Garnier <garnier@mit.edu>
2023-07-05 10:50:32 -0400

    remove numba warning when running coverage tests

--- src/inductance/_numba.py
+++ src/inductance/_numba.py
@@ -3,24 +3,21 @@
 import os
 
 # try to enable use without numba.  Those with guvectorize will not work.
+
 try:
     from numba import guvectorize, jit, njit, prange
 
     # if numba is diabled, redfine the jit decorator to do nothing
-    # so that coverage testing will work
     if os.getenv("NUMBA_DISABLE_JIT", "0") == "1":  # pragma: no cover
         raise ImportError
 
 except ImportError:  # pragma: no cover
     from inspect import currentframe, getframeinfo
-    from warnings import warn_explicit
+    from warnings import warn, warn_explicit
 
-    _WARNING = (
-        "Numba disabled. Mutual inductance calculations "
-        + "will not be accelerated and some API will not be available."
-    )
-    _finfo = getframeinfo(currentframe())  # type: ignore
-    warn_explicit(_WARNING, RuntimeWarning, _finfo.filename, _finfo.lineno)
+    if not os.getenv("COVERAGE_RUN", ""):  # pragma: no cover
+        _WARNING = "Numba acceleration disabled. Some API will not be available."
+        warn(_WARNING, RuntimeWarning)
 
     def _jit(*args, **kwargs):
         if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
@@ -32,9 +29,10 @@ except ImportError:  # pragma: no cover
 
     def _guvectorize(*args, **kwds):
         def fake_decorator(f):
-            warning = f"{f.__name__} requires Numba JIT."
-            finfo = getframeinfo(currentframe().f_back)  # type: ignore
-            warn_explicit(warning, RuntimeWarning, finfo.filename, finfo.lineno)
+            if not os.getenv("COVERAGE_RUN", ""):  # pragma: no cover
+                warning = f"{f.__name__} requires Numba JIT."
+                finfo = getframeinfo(currentframe().f_back)  # type: ignore
+                warn_explicit(warning, RuntimeWarning, finfo.filename, finfo.lineno)
             return lambda f: None
 
         return fake_decorator
@@ -43,3 +41,6 @@ except ImportError:  # pragma: no cover
     njit = _jit
     prange = range
     jit = _jit
+
+
+__all__ = ["guvectorize", "jit", "njit", "prange"]
