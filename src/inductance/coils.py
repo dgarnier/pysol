50d74e1ad86f29b0059bfe21eca32c0873d398f4
Darren Garnier <garnier@mit.edu>
2023-07-05 23:21:29 -0400

    coils API

--- src/inductance/coils.py
+++ src/inductance/coils.py
@@ -10,13 +10,15 @@ Filaments are defined as an numpy 3 element vector
 """
 from __future__ import annotations
 
+from dataclasses import dataclass
+from enum import Enum
+
 import numpy as np
 
 from .filaments import (
     filament_coil,
     mutual_inductance_of_filaments,
     radial_force_of_filaments,
-    self_inductance_by_filaments,
     vertical_force_of_filaments,
 )
 from .self import (
@@ -27,13 +29,39 @@ from .self import (
     L_lyle6_appendix,
     L_maxwell,
     dLdR_lyle6,
+    self_inductance_by_filaments,
 )
+
+
+class Shape(Enum):
+    """Enum for conductor shapes."""
+
+    round = "round"
+    hollow_round = "hollow_round"
+    rect = "rectangular"
+
+
+@dataclass
+class Conductor:
+    """Conductor object to keep track of conductor parameters."""
+
+    shape: Shape | str = Shape.round
+    r: float = 0.0
+    dr: float = 0.0
+    dz: float = 0.0
+    r_i: float = 0.0
+    rho: float = 0.0
+
+    def __post_init__(self):
+        """Post init."""
+        if isinstance(self.shape, str):
+            self.shape = Shape(self.shape)
 
 
 class Coil:
     """Rectangular coil object to keep track of coil parameters."""
 
-    def __init__(self, r, z, dr, dz, nt=1, at=1, nr=0, nz=0):
+    def __init__(self, r, z, dr, dz, nt=1, at=1, nr=0, nz=0, theta=0, **kwargs):
         """Create a rectangular coil object.
 
         Args:
@@ -41,10 +69,11 @@ class Coil:
             z (float): vertical center of coil
             dr (float): radial width of coil
             dz (float): axial height of coil
-            nt (int): number of turns in coil
+            nt (int, optional): number of turns in coil
             nr (int, optional): Number of radial sections to filament coil. Defaults to 0.
             nz (int, optional): Number of axial sections to filament coil. Defaults to 0.
             at (float, optional): Amperage of coil. Defaults to 0.
+            theta (float, optional): Rotation angle in radians. Defaults to 0.
         """
         self.r = r
         self.z = z
@@ -52,7 +81,12 @@ class Coil:
         self.dz = dz
         self.nt = nt
         self.at = at
+        self.theta = theta
         self.fils = None
+
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
         if (nr > 0) and (nz > 0):
             self.nr = nr
@@ -90,11 +124,32 @@ class Coil:
     def z2(self):  # noqa: D102
         return self.z + self.dz / 2
 
+    def L_best(self):
+        """Inductance by best formula."""
+        if self.dz < 0.2 * self.r:
+            # Lyle's formula is not valid for long coils
+            # as its an expansion in dz/r
+            return self.L_Lyle6()
+        else:
+            return self.L_filament()
+
+    @property
+    def L(self):  # noqa: D102
+        if self._L is None:
+            self._L = self.L_best()
+        return self._L
+
+    @L.setter
+    def L(self, value):
+        self._L = value
+
     def filamentize(self, nr, nz):
         """Create an array of filaments to represent the coil."""
         self.nr = nr
         self.nz = nz
-        self.fils = filament_coil(self.r, self.z, self.dr, self.dz, self.nt, nr, nz)
+        self.fils = filament_coil(
+            self.r, self.z, self.dr, self.dz, self.nt, nr, nz, theta=self.theta
+        )
 
     def L_Maxwell(self):
         """Inductance by Maxwell's formula."""
@@ -112,11 +167,26 @@ class Coil:
         """Inductance by Lyle's formula, 6th order, appendix."""
         return L_lyle6_appendix(self.r, self.dr, self.dz, self.nt)
 
-    def L_filament(self):
+    def L_filament(self, nr=0, nz=0):
         """Inductance by filamentation."""
-        return self_inductance_by_filaments(
-            self.fils, conductor="rect", dr=self.dr / self.nr, dz=self.dz / self.nz
-        )
+        if nr != 0 and nz != 0:
+            self.filamentize(nr, nz)
+        cond = getattr(self, "conductor", None)
+        if cond is None:
+            return self_inductance_by_filaments(
+                self.fils,
+                conductor=Shape.rect.name,
+                dr=self.dr / self.nr,
+                dz=self.dz / self.nz,
+            )
+        elif "round" in cond.shape.value:
+            return self_inductance_by_filaments(
+                self.fils, conductor=cond.shape.name, a=cond.r
+            )
+        elif "rect" in cond.shape.value:
+            return self_inductance_by_filaments(
+                self.fils, conductor=cond.shape.name, dr=cond.dr, dz=cond.dz
+            )
 
     def L_long_solenoid_butterworth(self):
         """Inductance by Butterworth's formula."""
@@ -158,6 +228,25 @@ class CompositeCoil(Coil):
         self.coils = coils
         self.nt = sum(coil.nt for coil in coils)
         self.at = sum(coil.at for coil in coils)
-        self.r = sum(coil.r * coil.nt for coil in coils) / self.nt
-        self.z = sum(coil.z * coil.nt for coil in coils) / self.nt
+        self.r_c = sum(coil.r * coil.nt for coil in coils) / self.nt
+        r1 = min(coil.r - coil.dr for coil in coils)
+        r2 = max(coil.r + coil.dr for coil in coils)
+        self.r = (r1 + r2) / 2
+        self.dr = r2 - r1
+        z1 = min(coil.z - coil.dz for coil in coils)
+        z2 = max(coil.z + coil.dz for coil in coils)
+        self.z = (z1 + z2) / 2
+        self.dz = z2 - z1
+        self.z_c = sum(coil.z * coil.nt for coil in coils) / self.nt
         self.fils = np.concatenate([coil.fils for coil in coils])
+
+    def L_best(self):
+        """Inductance of composite coils by best formula."""
+        L = 0
+        for i, c1 in enumerate(self.coils):
+            for j, c2 in enumerate(self.coils):
+                if i == j:
+                    L += c1.L
+                else:
+                    L += c1.M_filament(c2)
+        return L
